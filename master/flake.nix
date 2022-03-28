4bcc3f4165e63b280566688df2330c0a14c4787a
hugosenari <hugosenari@gmail.com>
2022-03-27 21:49:22 -0300

    chore: re index eth_common

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-eth_common-master.ref   = "refs/heads/master";
   inputs.src-eth_common-master.owner = "status-im";
   inputs.src-eth_common-master.repo  = "nim-eth-common";
-  inputs.src-eth_common-master.dir   = "";
   inputs.src-eth_common-master.type  = "github";
   
   inputs."rlp".owner = "nim-nix-pkgs";
